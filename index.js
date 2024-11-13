b9d104691e3bde669909dfb3574f4bd0e1a84710
Frankiegit config --global user.name Frankiegit config --global user.name Frankie <Frankiebrown996@gmail.com>
2024-11-13 14:30:57 +0000

    trying

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let rpassword1 = document.getElementById("passwordOne")
+let rpassword2 = document.getElementById("passwordTwo")
+
 function randomPassword1(){
     let password1 = "";
     for (let i = 0; i < 15; i++) {
@@ -10,12 +13,13 @@ function randomPassword1(){
 }
 
 
-
-
-function randomPassword2(){
-    let password2 = "";
-    for (let i=0;i<15;i++){
-        password+=characters[Math.floor(Math.random()*characters.length)];
+function randomPassword2() {
+    let password2 = "";  // Initialize the empty string for the password
+    
+    for (let i = 0; i < 15; i++) {
+        password2 += characters[Math.floor(Math.random() * characters.length)];  // Use password2 instead of password
     }
+    
+    rpassword2.textContent = password2;  // Corrected rpassword2 instead of rpassword12
     return password2;
 }
